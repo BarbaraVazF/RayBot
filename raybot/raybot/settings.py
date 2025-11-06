36168894f5ffb6ec4d463cdf99868edd0b6cedd9
andrecastrom06 <alcms@cesar.school>
2025-11-06 15:57:18 -0300

    fix: correção banco de dados para local

--- raybot/raybot/settings.py
+++ raybot/raybot/settings.py
@@ -58,7 +58,10 @@ WSGI_APPLICATION = 'raybot.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600)
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
